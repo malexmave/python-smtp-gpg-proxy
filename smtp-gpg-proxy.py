4dbb19c2172dcfab3f02ff04a40881c1c7ca7c46
Max Maass <max@velcommuta.de>
2014-03-17 12:07:40 +0100

    Changed over to secure_smtpd for SSL-encrypted SMTP. Added some parser magic. At the moment, it will relay the messages unchanged.

--- smtp-gpg-proxy.py
+++ smtp-gpg-proxy.py
@@ -1,16 +1,41 @@
-from datetime import datetime
 import asyncore
-from smtpd import SMTPServer
 
-class GPGServer(SMTPServer):
+from email.parser import Parser
+from secure_smtpd import ProxyServer
+
+class GPGServer(ProxyServer):
     no = 0
     def process_message(self, peer, mailfrom, rcpttos, data):
-        print data
         self.no += 1
+        msg = Parser().parsestr(data)
+        print "MSG #%i" % self.no
+        print "From: %s" % msg['from']
+        print "To: %s" % msg['to']
+        print "Subject: %s" % msg['subject']
+
+        attachments = {}
+        body = ""
+        cnt = 0
+        for part in msg.walk():
+            if part.get_content_maintype() == "multipart":
+                continue
+            filename = part.get_filename()
+            if filename != None:
+                cnt += 1
+                print "Attachment %i: %s" % (cnt, filename)
+                attachments[filename] = part.get_payload(decode=True)
+            else:
+                print "Mail body found."
+                body = part.get_payload(decode=True)
+                # body += "\n\n--\nModified by Python."
+                # part.set_payload(body)
+        data = str(msg)[str(msg).find("\n")+1:] # TODO: Convert to non-horrible syntax
+
+        ProxyServer.process_message(self, peer, mailfrom, rcpttos, data)
 
 
-def run():
-    foo = GPGServer(('localhost', 25), None)
+def run(remotesrv, remoteport):
+    foo = GPGServer(('localhost', 25), (remotesrv, remoteport), ssl_out_only=True)
     try:
         asyncore.loop()
     except KeyboardInterrupt:
@@ -18,4 +43,6 @@ def run():
 
 
 if __name__ == '__main__':
-	run()
+    remotesrv = raw_input("Please enter the remote server address (smtp.strato.de): ")
+    remoteport = int(raw_input("Please enter the remote server port (465): "))
+    run(remotesrv, remoteport)
